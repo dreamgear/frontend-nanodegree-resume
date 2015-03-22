0401dddb860b966fabb2879da54d7a25f587fc14
jeff <jeff@seamanpaper.com>
2015-03-22 16:24:37 -0400

    Completed the four object definitions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,48 +1,74 @@
-  var email = "dreamgear@gmail.com";
-  
-  bio = {
-		  "name" : "Jeff Dickens",
-		  "role" : "IT Manager",
-		  "contacts" : {
-			  "email" : "dreamgear@gmail.com",
-			  "phone" : "978-297-1313",
-			  "github" : "dreamgear",
-			  "location" : "Winchendon, MA"
-		  },		  
-		  "bioPic" : "http://www.dreamgearweb.com/images/avatar.jpg",
-		  "welcomeMessage" : "Welcome to my Interactive Resume",
-		  "skills" : [ "IT Management", "javascript", "jQuery", "PHP",
-			"UNIX system administration"]
-  };
-  
-  work = {};
-  work.position = "IT Director";
-  work.employer = "Seaman Paper";
-  work.years = 9;
-  work.city = "Gardner, MA";
-  
-  education = {};
-  education["name"] = "Fitchburg State University";
-  education["years"] = "2008-2012";
-  education["city"] = "Fitchburg, MA";
-  
-  var name = "Jeff Dickens";
-  var role = "IT Manager";
-  
-  var formattedName = HTMLheaderName.replace("%data%",bio.name);
-  var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
-  
-  $("#header").append(HTMLheaderName.replace("%data%",bio.name));
-  $("#header").append(HTMLheaderRole.replace("%data%",bio.role));
-  $("#header").append(HTMLcontactGeneric.replace("%contact%",'phone').replace("%data%",bio.contact));
-  $("#header").append(HTMLemail.replace("%data%",bio.email));
-  $("#header").append(HTMLbioPic.replace("%data%",bio.bioPic));
-  $("#header").append(HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage));
-  $("#header").append(HTMLskills.replace("%data%",bio.skills));
- 
-  $("#main").append(work["position"]);
-  $("#main").append(education.name);
-  
-  
-  
-  console.log("Email: " + email);
+var bio = {
+	"name" : "Jeff Dickens",
+	"role" : "IT Manager",
+	"contacts" : {
+		"mobile" : "",
+		"email" : "dreamgear@gmail.com",
+		"github" : "dreamgear",
+		"twitter" : "dreamgearjd",
+		"location" : "Winchendon, MA"
+	},
+	"welcomeMessage" : "Welcome to my Interactive Resume",
+	"skills" : [ "IT Management", "javascript", "jQuery", "PHP",
+			"UNIX system administration" ],
+	"bioPic" : "http://www.dreamgearweb.com/images/avatar.jpg"
+};
+
+var education = {
+	"schools" : [ {
+		"name" : "Fitchburg State University",
+		"locaion" : "Fitchburg, MA, USA",
+		"degree" : "BS",
+		"majors" : [ "Computer Science" ],
+		"year" : 2012,
+		"url" : "http://www.fitchburgstate.edu",
+		"honors" : "Summa cum Laude, Alpha Sigma Lambda"
+	} ],
+	"onlineCourses" : [ {
+		"title" : "Javascript Basics",
+		"school" : "Udacity",
+		"date" : 2015,
+		"url" : "https://www.udacity.com/course/ud804"
+	} ]
+};
+
+var work = {
+	"jobs" : [
+			{
+				"employer" : "Seaman Paper Company",
+				"title" : "IT Manager",
+				"dates" : "2006-Present",
+				"location" : "Gardner, MA",
+				"description" : "Built Infrastructure for Growing Manufacturer"
+			},
+			{
+				"employer" : "Self-Employed",
+				"title" : "Web Designer/Programmer",
+				"dates" : "2002-2005",
+				"location" : "Winchendon, MA",
+				"description" : "Learned Linux, Apache, MySQL & PHP; Deployed Content Management Systems"
+			},
+			{
+				"employer" : "ERS International",
+				"title" : "Software Tools Engineer",
+				"dates" : "1999-2001",
+				"location" : "Boxborough, MA",
+				"description" : "Managed UNIX/Windows toolchains for startup."
+			},
+			{
+				"employer" : "Digital Equipment Corporation",
+				"title" : "Software Tools Engineer",
+				"dates" : "1980-1998",
+				"location" : "Littleton, MA",
+				"description" : "Held several engineering support positions during DEC's heyday"
+			} ]
+};
+
+var projects = {
+	"projects" : [ {
+		"title" : "Roll Labeling System",
+		"dates" : "2012-2015",
+		"description" : "Multifaceted System to track rolls of paper: a NetSuite ERP integration",
+		"images" : [ "", "" ]
+	} ]
+};
